d591b62b6ede77dc7b20cfc139609cca97bc80a9
selinasein <neulmisscj@gmail.com>
2023-04-24 13:08:14 -0700

    update fakedb

--- fake-db.ts
+++ fake-db.ts
@@ -5,7 +5,7 @@ const users = [
     email: "samsmith@gmail.com",
     password: "sam123",
     following: ["3", "4", "5"],
-    bucket_messages: [1],
+    message: "1001"
   },
   {
     id: "2",
@@ -13,7 +13,7 @@ const users = [
     email: "janedoe@gmail.com",
     password: "jane123",
     following: ["1", "4", "7"],
-    posts: ["1002", "1003"],
+    message: ["1002", "1003"],
   },
   {
     id: "3",
@@ -21,7 +21,7 @@ const users = [
     email: "johnbrown@gmail.com",
     password: "john123",
     following: ["1", "2"],
-    posts: ["1004"],
+    message: ["1004"],
   },
   {
     id: "4",
@@ -29,7 +29,7 @@ const users = [
     email: "annalee@gmail.com",
     password: "anna123",
     following: ["1", "2", "6"],
-    posts: ["1005"],
+    message: ["1005"],
   },
   {
     id: "5",
@@ -37,7 +37,7 @@ const users = [
     email: "davidc@gmail.com",
     password: "david123",
     following: ["1"],
-    posts: ["1006"],
+    message: ["1006"],
   },
   {
     id: "6",
@@ -45,7 +45,7 @@ const users = [
     email: "mikejones@gmail.com",
     password: "mike123",
     following: ["4", "7"],
-    posts: ["1007", "1008"],
+    message: ["1007", "1008"],
   },
   {
     id: "7",
@@ -53,7 +53,7 @@ const users = [
     email: "jessicas@gmail.com",
     password: "jessica123",
     following: ["2", "6"],
-    posts: ["1009"],
+    message: ["1009"],
   },
 ];
 
@@ -96,65 +96,54 @@ const filters = [
   },
 ];
 
-const progress = {
-  "0": "In progress",
-  "1": "Completed",
-};
-
-const bucket_messages = [
-  {
-    id: 1,
-    bucket_id: 1,
-    post_id: 1001,
-  },
-];
-
 const buckets = [
   {
     id: "1",
-    message: "Ride a roller coaster",
+    title: "Ride a roller coaster",
     filter: ["2", "5"],
   },
   {
     id: "2",
-    message: "Run a marathon",
+    title: "Run a marathon",
     filter: ["8"],
   },
   {
     id: "3",
-    message: "Mentor someone",
+    title: "Mentor someone",
     filter: ["4"],
   },
   {
     id: "4",
-    message: "Visit friends",
+    title: "Visit friends",
     filter: ["5"],
   },
   {
     id: "5",
-    message: "Go skiing",
+    title: "Go skiing",
     filter: ["2", "5", "8"],
   },
   {
     id: "6",
-    message: "Adopt a pet",
+    title: "Adopt a pet",
     filter: ["9"],
   },
   {
     id: "7",
-    message: "Go fishing",
+    title: "Go fishing",
     filter: ["5"],
   },
   {
     id: "8",
-    message: "Go swimming",
+    title: "Go swimming",
     filter: ["5", "8"],
   },
 ];
 
-const post = [
+const message = [
   {
     id: 1001,
+    user_id: 1,
+    bucket_id: 1,
     message:
       "I am going to mentor someone! I am so excited to help other students!",
     timestamp: Date.now(),
