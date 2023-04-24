f274f1c92f1d7cbbe8b1ff7f45759bfca152b5fd
Fernando <fernando.daumichen@gmail.com>
2023-04-24 13:52:37 -0700

    creation of the routes =P

--- server.js
+++ server.js
@@ -1,56 +1,26 @@
 const express = require("express");
 const app = express();
-const database = require("./database");
+const home = require("./routes/home");
+const feeds = require("./routes/feeds");
+const createPost = require("./routes/createPost");
+const deletePost = require("./routes/deletePost");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    numberOfItterations: 50,
-  });
-});
-
-app.get("/feeds", (req, res) => {
-  const feeds = database.getFeeds();
-  res.render("feeds.ejs", {
-    feeds,
-  });
-});
-
-app.get("/feeds/:id", (req, res) => {
-  const id = +req.params.id;
-  const feed = database.getFeed(id);
-  if (!feed) {
-    res.status(404).render("feed404.ejs");
-    return;
-  }
-  res.render("singleFeed.ejs", {
-    feed,
-  });
-});
-
-app.get("/createPost", (req, res) => {
-  res.render("createPost.ejs");
-});
-
-app.post("/feeds", (req, res) => {
-  const data = req.body;
-  database.addFeed(data);
-
-  res.redirect("/feeds");
-});
-
-app.post("/feeds/:id/delete", (req, res) => {
-  const id = +req.params.id;
-  database.deleteFeed(id);
-
-  res.redirect("/feeds");
-});
-
 app.use(express.static("public"));
 
-const port = 8080;
+app.use("/", home); // Add this line back
+app.use("/feeds", feeds);
+app.use("/createPost", createPost);
+app.use("/deletePost", deletePost);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+const port = 8000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
