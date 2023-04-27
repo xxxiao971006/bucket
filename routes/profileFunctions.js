cf8cd2135f59f08552c8d435d88dd47e344132b0
selinasein <neulmisscj@gmail.com>
2023-04-27 11:55:09 -0700

    clean

--- routes/profileFunctions.js
+++ routes/profileFunctions.js
@@ -1,38 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {getUserFeed, getMessageByMessageId, getUsernameById, getUserInfoByUsername} = require("../fake-db");
+const {getUserFeed} = require("../fake-db");
 
 router.get("/", (req, res) => {
-  // console.log("Rendering profile.ejs"); // for debugging
   const data = getUserFeed("1");
   res.render("profile", { data });
 
 });
 
 module.exports = router;
-
-/*
-<user.message.forEach((messageId) => { %>
-  <% const message = database.getMessageById(messageId); %>
-  <% if (message) { %>
-    <div class="post">
-      <h3><%= user.username %></h3>
-      <p><%= message.message %></p>
-      <div class="post-actions">
-        <button class="like-button" data-message-id="<%= message.id %>">
-          <i class="fas fa-heart"></i>
-          <% if (message.likes) { %>
-            <span class="likes-count"><%= message.likes %></span>
-          <% } %>
-        </button>
-        <button class="comment-button">
-          <i class="fas fa-comment"></i>
-          <% if (message.comments && message.comments.length) { %>
-            <span class="comments-count"><%= message.comments.length %></span>
-          <% } %>
-        </button>
-      </div>
-    </div>
-  <% } %>
-<% }) %>
-*/
