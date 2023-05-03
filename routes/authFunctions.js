0bcecc202aef6a07e1ab9eaa3782d4044fbbc5a8
selinasein <neulmisscj@gmail.com>
2023-05-02 23:22:40 -0700

    prisma changes

--- routes/authFunctions.js
+++ routes/authFunctions.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const passport = require("../passport-middleware");
 const { forwardAuthenticated } = require("../middleware");
 const { createUser } = require("../fake-db");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 router.get("/login", forwardAuthenticated, (req, res) => res.render("login"));
 
@@ -10,8 +12,11 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   const user = createUser(req.body);
+    //   const user = await prisma.user.create({
+    //   data: req.body
+    // });
   if (user) {
     res.redirect("/auth/login");
   } else {
@@ -30,6 +35,30 @@ router.post(
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/auth/login");
+});
+
+router.get("/prismatest", async (req, res) => {
+  try {
+    // const user = await prisma.user.create({
+    //   data: {
+    //     username: "selina",
+    //     password: "123",
+    //     email: "selina@gmail.com",
+    //   },
+    // });
+    const user = await prisma.user.findUnique({
+      where: {
+        username: "samsmith",
+      },
+      include: { buckets: true },
+    });
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect();
+  } finally {
+    await prisma.$disconnect();
+  }
 });
 
 module.exports = router;
