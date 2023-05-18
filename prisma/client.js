8a117abf34077800152263947eace3e4eb7f101f
Xiao Zhang <xxxiao971006@gmail.com>
2023-05-18 16:25:30 -0700

    add db url

--- prisma/client.js
+++ prisma/client.js
@@ -1,5 +1,11 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: "postgresql://johndoe:randompassword@localhost:5432/mydb?schema=public",
+        },
+    },
+});
 
 module.exports = prisma;
